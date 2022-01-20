c53239cd8f555c6f5725843822727749a9b5ca05
TS-DOS <haramoto.yuri@vega-c.com>
2022-01-20 21:11:20 +0900

    2 files modified

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-interface ActionSheetAttributes extends JSXBase.HTMLAttributes<HTMLElement> {}
+ReactDOM.render(<App />, document.getElementById('main'));
